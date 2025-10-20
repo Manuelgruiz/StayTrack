43c85c23781e8c7ecab40810cd136ac4219aadba
MANUE GARCÍA RUIZ <manuelgruiz22@gmail.com>
2025-10-20 17:36:56 +0200

    conf: Connection to database  #6

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from .routers import users, foods, exercises
+from .database import Base, engine
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="StayTrack API", version="0.1.0")
+
+app.include_router(users.router)
+app.include_router(foods.router)
+app.include_router(exercises.router)
 
 @app.get("/")
 def read_root():
